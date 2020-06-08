f14be92d93c34320e87805d7595eefe3cf76d642
ntkjer <ntkjer@gmail.com>
2020-06-08 11:01:08 -0400

    closest pair and furthest pair working

--- chapter1/closestpair/main.go
+++ chapter1/closestpair/main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math"
+	"sort"
 )
 
 func main() {
-	fmt.Println("")
+	nums := []float64{-12, 4, -1, 8, 6, 2, -3, 13}
+	fmt.Println(ClosestPair(nums))
+
 }
 
-func ClosestPair(nums []int) (int, int) {
-	startPos := rand.Intn(len(nums))
-	x, y := nums[startPos], nums[len(nums)-startPos]
-	for _, num := range nums {
-		if num < x {
-			x = num
-		} else if num < y && num > x {
-			y = num
+func ClosestPair(nums []float64) (float64, float64) {
+	var a, b, diff float64
+	diff = 10000000000000000000000000000000000000
+	sort.Float64s(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if math.Abs(nums[i]-nums[i+1]) < diff {
+			diff = math.Abs(nums[i] - nums[i+1])
+			a, b = nums[i], nums[i+1]
 		}
 	}
-	return x, y
+	return a, b
 }
