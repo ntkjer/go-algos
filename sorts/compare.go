c3c4a2036c19bcbddf17dcb079a8fa8e00c1781c
ntkjer <ntkjer@gmail.com>
2020-06-14 17:14:54 -0400

    working legends and multigraph

--- sorts/compare.go
+++ sorts/compare.go
@@ -10,6 +10,8 @@ import (
 	"github.com/ntkjer/sedgewick/sorts/insertion"
 	"github.com/ntkjer/sedgewick/sorts/selection"
 	"github.com/ntkjer/sedgewick/sorts/shell"
+
+	"github.com/ntkjer/sedgewick/utils/chart"
 	"github.com/ntkjer/sedgewick/utils/stopwatch"
 )
 
@@ -51,14 +53,56 @@ func timedRandomInput(algo string, n, trials int) float64 {
 	return total
 }
 
+func step(n int) []float64 {
+	steps := 10.0
+	res := make([]float64, int(steps)+1)
+	total := 0.0
+	for i := 0; i < len(res); i++ {
+		res[i] = total
+		total += float64(n) / steps
+		fmt.Println(total)
+	}
+	return res
+}
+
+func generatePlotPoints(algoA, algoB string, n, trials int) ([]float64, []float64, []float64) {
+	//for a large n, lets step the number into a slice of floats as our x axis
+	xaxis := step(n)
+	fmt.Println(xaxis)
+
+	aTimes, bTimes := make([]float64, n), make([]float64, n)
+
+	for i, num := range xaxis {
+		time1 := timedRandomInput(algoA, int(num), trials)
+		time2 := timedRandomInput(algoB, int(num), trials)
+		aTimes[i] = time1
+		bTimes[i] = time2
+	}
+
+	return xaxis, aTimes, bTimes
+}
+
 func main() {
 	algoA, algoB, n, t := os.Args[1], os.Args[2], os.Args[3], os.Args[4]
 	N, _ := strconv.Atoi(n)
 	trials, _ := strconv.Atoi(t)
+
 	time1 := timedRandomInput(algoA, N, trials)
 	time2 := timedRandomInput(algoB, N, trials)
+	//fmt.Println(time1, time2)
+
 	ratio := time2 / time1
+
 	fmt.Printf("For %d random floats\n      %s is", N, algoA)
 	fmt.Printf("  %.1f times faster than %s.\n", ratio, algoB)
+
+	xaxis, a, b := generatePlotPoints(algoA, algoB, N, trials)
+	c1 := plot.NewCoordinates(xaxis, a)
+	p := plot.NewPlot("algo runtimes")
+	c2 := plot.NewCoordinates(xaxis, b)
+	p.Add(c1, algoA)
+	p.Add(c2, algoB)
+	p.AddLegend()
+	p.Output("graph.png")
 
 }
