9c81f80b042bf519253e17ced3fce2aff83e9ddf
ntkjer <ntkjer@gmail.com>
2020-06-16 18:39:24 -0400

    slight improvements to top down mergesort

--- sorts/merge/sort.go
+++ sorts/merge/sort.go
@@ -3,6 +3,8 @@ package merge
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/ntkjer/sedgewick/sorts/insertion"
 )
 
 //TODO: Generics and Comparable
@@ -18,6 +20,80 @@ func NewSorter(input []interface{}) *Sorter {
 	s.items = input
 	s.sorted = false
 	return s
+}
+
+func (s Sorter) IsSorted(input []interface{}) bool {
+	for i := 1; i < len(input); i++ {
+		if s.Less(input[i], input[i-1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func (s Sorter) Show(input []interface{}) {
+	for _, val := range input {
+		fmt.Println(val)
+	}
+}
+
+func (s Sorter) Sort(input []interface{}) {
+	aux := make([]interface{}, len(input))
+	s.aux = aux
+	s.sort(input, 0, len(input)-1)
+}
+
+func (s Sorter) sort(input []interface{}, lo, hi int) {
+	if hi <= lo {
+		return
+	}
+
+	//Insertion sort for small subarrays will give us a ~10-15% boost
+	if hi <= 15 {
+		var x *insertion.Sorter = new(insertion.Sorter)
+		x.Sort(input[:hi])
+	}
+
+	mid := lo + (hi-lo)/2
+	s.sort(input, lo, mid)
+	s.sort(input, mid+1, hi)
+
+	if s.Less(input[mid], input[mid+1]) {
+		return
+	}
+
+	s.merge(input, lo, mid, hi)
+}
+
+func (s Sorter) merge(input []interface{}, lo, mid, hi int) {
+
+	i, j := lo, mid+1
+
+	for k := lo; k <= hi; k++ {
+		s.aux[k] = input[k]
+	}
+
+	for k := lo; k <= hi; k++ {
+		if i > mid {
+			input[k] = s.aux[j]
+			j += 1
+		} else if j > hi {
+			input[k] = s.aux[i]
+			i += 1
+		} else if s.Less(s.aux[j], s.aux[i]) {
+			input[k] = s.aux[j]
+			j += 1
+		} else {
+			input[k] = s.aux[i]
+			i += 1
+		}
+	}
+}
+
+func exchange(input []interface{}, i, j int) {
+	tmp := input[i]
+	input[i] = input[j]
+	input[j] = tmp
 }
 
 //hacky way for now
@@ -51,66 +127,4 @@ func (s Sorter) Less(x, y interface{}) bool {
 	}
 
 	return true
-}
-
-func (s Sorter) IsSorted(input []interface{}) bool {
-	for i := 1; i < len(input); i++ {
-		if s.Less(input[i], input[i-1]) {
-			return false
-		}
-	}
-	return true
-}
-
-func (s Sorter) Show(input []interface{}) {
-	for _, val := range input {
-		fmt.Println(val)
-	}
-}
-
-func (s Sorter) Sort(input []interface{}) {
-	aux := make([]interface{}, len(input))
-	s.aux = aux
-	s.sort(input, 0, len(input)-1)
-}
-
-func (s Sorter) sort(input []interface{}, lo, hi int) {
-	if hi <= lo {
-		return
-	}
-	mid := lo + (hi-lo)/2
-	s.sort(input, lo, mid)
-	s.sort(input, mid+1, hi)
-	s.merge(input, lo, mid, hi)
-}
-
-func (s Sorter) merge(input []interface{}, lo, mid, hi int) {
-
-	i, j := lo, mid+1
-
-	for k := lo; k <= hi; k++ {
-		s.aux[k] = input[k]
-	}
-
-	for k := lo; k <= hi; k++ {
-		if i > mid {
-			input[k] = s.aux[j]
-			j += 1
-		} else if j > hi {
-			input[k] = s.aux[i]
-			i += 1
-		} else if s.Less(s.aux[j], s.aux[i]) {
-			input[k] = s.aux[j]
-			j += 1
-		} else {
-			input[k] = s.aux[i]
-			i += 1
-		}
-	}
-}
-
-func exchange(input []interface{}, i, j int) {
-	tmp := input[i]
-	input[i] = input[j]
-	input[j] = tmp
 }
